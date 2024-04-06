3d169477aefe95a601c26985baa49cfc82afea6a
= <=>
2024-04-06 12:34:57 +0200

    refactor

--- src/show/index.ts
+++ src/show/index.ts
@@ -1,7 +1,7 @@
-import { JSX } from 'react';
+import { JSX, FC } from 'react';
 import { ShowProps } from './type';
 
-export const Show = (props: ShowProps): JSX.Element | null => {
+export const Show: FC<ShowProps> = (props): JSX.Element | null => {
   if (props.when) {
     if (typeof props.children === 'function') {
       return props.children();
