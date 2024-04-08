7372a920c6f1fa03a018813f63031d23f13a0dd5
= <=>
2024-04-08 22:20:25 +0200

    chore - for component fallback

--- src/for/index.ts
+++ src/for/index.ts
@@ -1,6 +1,9 @@
 import { JSX } from 'react';
 import { ForProps } from './type';
-  
-export const For = <T, K extends JSX.Element>(props: ForProps<T, K>): K[] => {
-  return props.each.map(props.children);
+
+export const For = <T, K extends JSX.Element>(
+  props: ForProps<T, K>
+): K[] | JSX.Element | null => {
+  if (props.each?.length) return props.each.map(props.children);
+  else return props.fallback ?? null;
 };
--- src/for/type.ts
+++ src/for/type.ts
@@ -3,4 +3,5 @@ import { JSX } from "react";
 export interface ForProps<T, K extends JSX.Element> {
   each: readonly T[];
   children: (item: T, index: number) => K;
+  fallback?: JSX.Element;
 }
